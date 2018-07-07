cb71710eb468bb9b8b7c5efda8548bf940488b80
RebotXP <1005110192@qq.com>
2018-07-07 15:39:27 +0800

    1.优化编码问题

--- src/main/java/com/ast/controller/ActiveManger.java
+++ src/main/java/com/ast/controller/ActiveManger.java
@@ -36,11 +36,12 @@ public class ActiveManger {
     @RequestMapping(value = "/ActiveAdd",method = RequestMethod.GET)
     public int activeAdd(HttpServletRequest request,HttpServletResponse response)throws Exception{
         _addResponseHead(response);
-        String activeHead=  new String(request.getParameter("activeHead").getBytes("ISO-8859-1"),"UTF-8") ;
+        String activeHead=  new String(request.getParameter("activeHead").getBytes("ISO-8859-1"),"GBK") ;
         String activeBody=request.getParameter("activeBody");
         String activeAuthor=request.getParameter("activeAuthor");
         int activeUserCount=Integer.parseInt(request.getParameter("activeUserCount") == null ? "0" : request.getParameter("activeUserCount"));
         int activeCount=Integer.parseInt(request.getParameter("activeCount") == null ? "1" : request.getParameter("activeCount"));
+        PrintlnLog("Info: "+"中文测试");
         PrintlnLog("Info: "+"activeHead "+activeHead);
         PrintlnLog("Info: "+"activeBody "+activeBody);
         PrintlnLog("Info: "+"activeAuthor "+activeAuthor);
