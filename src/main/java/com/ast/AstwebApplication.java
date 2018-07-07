42639f1ffb9429eebf00ac6346d4b128a02e4ada
RebotXP <1005110192@qq.com>
2018-07-07 15:20:58 +0800

    1.优化编码问题

--- src/main/java/com/ast/AstwebApplication.java
+++ src/main/java/com/ast/AstwebApplication.java
@@ -3,13 +3,25 @@ package com.ast;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication()
 public class AstwebApplication extends SpringBootServletInitializer {
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setForceEncoding(true);
+        characterEncodingFilter.setEncoding("UTF-8");
+        registrationBean.setFilter(characterEncodingFilter);
+        return registrationBean;
+    }
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
